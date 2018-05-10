6a1e7cee5236f112567887e13024b87b7706c2df
Tung Uyen <uyen.do@apps-cyclone.com>
2018-05-10 17:28:03 +0700

    call api and update UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,53 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRouters } from "./app.routers";
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AppRouters} from './app.routers';
-import { HeaderComponent } from './components/header/header.component';
-import { Footer1Component } from './components/footer1/footer1.component';
-import { Footer2Component } from './components/footer2/footer2.component';
-import { Footer3Component } from './components/footer3/footer3.component'
 
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { Footer1Component } from './components/footer-1/footer-1.component';
+import { Footer2Component } from './components/footer-2/footer-2.component';
+
+import { NewsComponent } from './news/news.component';
+import { NewsDetailComponent } from './news/news-detail/news-detail.component';
+import { NewsMainComponent } from './news/news-main/news-main.component';
+import { PhotosComponent } from './photos/photos.component';
+import { VideosComponent } from './videos/videos.component';
+import { TeamsComponent } from './teams/teams.component';
+import { DestinationComponent } from './destination/destination.component';
+
+import { MatchesComponent } from './matches/matches.component';
+import { GroupsComponent } from './groups/groups.component';
+
+
+import { SharedModule} from "./share/share.module";
 @NgModule({
+  //import component child
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent,
-    Footer1Component,
+    FooterComponent,
+    PhotosComponent,
+    VideosComponent,
+    TeamsComponent,
+    DestinationComponent,
+    MatchesComponent,
+    GroupsComponent,
     Footer2Component,
-    Footer3Component
+    Footer1Component,
+    NewsDetailComponent,
+    NewsMainComponent,
+    NewsComponent
   ],
+  //import module parent 
   imports: [
+    SharedModule,
     BrowserModule,
-    AppRouters
+    AppRouters,
   ],
   providers: [],
   bootstrap: [AppComponent]
